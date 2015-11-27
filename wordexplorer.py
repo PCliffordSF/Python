1b5ff5b354b994d10b49e2f5e35f25f8a175d8c9
PCliffordSF <paulclifford2570@gmail.com>
2015-11-27 12:38:05 -0800

    11/25 inclass project

---
+++ wordexplorer.py
@@ -0,0 +1,41 @@
+#!/usr/local/bin/python3
+
+import cgi
+
+# program which interfaces python with http. 
+# program isn't useful, but good to keep around 
+
+filename = '/users/abrick/resources/insane-ascii'
+fh = open(filename)
+term = cgi.FieldStorage().getvalue('key') or 'goof'
+
+print("Content-type: text/html\n")
+
+contains = []
+containedBy = []
+
+for line in fh:
+    line = line.strip()
+    if term in line:
+        containedBy.append(line)       
+        #print('<a href=?insane=' + line + '>' + line + '</a><br/>')   
+    if line in term:
+        contains.append(line)
+        #print('<a href=?insane=' + line + '>' + line + '</a><br/>')
+        #print(' is contained by ' + term)
+
+fh.close()
+
+for word in containedBy:
+    print(term + ' is contained by ')
+    print('<a href=?key=' + word + '>' + word + '</a><br/>')
+    #print(word)
+
+for word in contains:
+    print(term + ' contains ')
+    print('<a href=?key=' + word + '>' + word + '</a><br/>')
+
+
+
+
+
